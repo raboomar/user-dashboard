d0c030ccbdeedeae7843a5aa31d1fe33c6281766
Rami <aboomar.2@osu.edu>
2022-07-24 21:32:38 -0400

    created user conroller

---
+++ users/src/main/java/com/example/users/api/UserController.java
@@ -0,0 +1,24 @@
+package com.example.users.api;
+
+import com.example.users.users.Gender;
+import com.example.users.users.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/users")
+@AllArgsConstructor
+public class UserController {
+
+    @CrossOrigin
+    @GetMapping
+    public List<User> getUsers(){
+        return List.of(new User(1L,"rami","rami@gmail", Gender.MALE));
+    }
+}
